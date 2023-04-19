0e9a3e03a54c4996dfc79a34d9b829e6ed0bc70e
Krishan2207 <131198963+Krishan2207@users.noreply.github.com>
2023-04-19 20:31:50 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st 
 
-st.write( "# Maximum Finder App")
+st.write( "#Largest Number Finder App")
 num1 = float(input("Enter the first number: "))
 num2 = float(input("Enter the second number: "))
 num3 = float(input("Enter the third number: "))
